e6ee1b7338ee84e176fec5edee5b51c2e139ee33
Drew Chase <dcmanproductions@gmail.com>
2023-09-16 16:54:26 -0400

    Add project files.

---
+++ Chase.CommonLib/FileSystem/Configuration/ConfigurationFile.cs
@@ -0,0 +1,101 @@
+﻿/*
+    Chase CommonLib - LFInteractive LLC. 2021-2024
+    CommonLib is a library of common functions and classes for .NET 5.0+.
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
+using Newtonsoft.Json;
+
+namespace Chase.CommonLib.FileSystem.Configuration;
+
+/// <summary>
+/// A configuration file is a json file that contains a collection of entries.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class ConfigurationFile<T> : IDisposable
+{
+    /// <summary>
+    /// The Configuration Files Content
+    /// </summary>
+    public T? Content { get; set; }
+
+    /// <summary>
+    /// If the Configuration File should be kept open or not
+    /// </summary>
+    public bool KeepOpen { get; private set; }
+
+    /// <summary>
+    /// The Configuration Files Path
+    /// </summary>
+    public string FilePath { get; private set; }
+
+    private FileStream? stream { get; set; }
+
+    /// <summary>
+    /// Creates or opens a configuration file.
+    /// </summary>
+    /// <param name="filePath"></param>
+    /// <param name="keepOpen"></param>
+    public ConfigurationFile(string filePath, bool keepOpen = false)
+    {
+        KeepOpen = keepOpen;
+        FilePath = filePath;
+        if (keepOpen)
+        {
+            stream = File.Open(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+        }
+    }
+
+    /// <summary>
+    /// Saves the contents of <see cref="Content">Content</see> to the configuration file.
+    /// </summary>
+    /// <returns></returns>
+    public bool Save()
+    {
+        if (KeepOpen)
+        {
+            stream ??= File.Open(FilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+            using StreamWriter writer = new(stream);
+            writer.Write(JsonConvert.SerializeObject(Content));
+            writer.Flush();
+            stream.Flush();
+            return true;
+        }
+        else
+        {
+            File.WriteAllText(FilePath, JsonConvert.SerializeObject(Content));
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Loads the contents of the configuration file into <see cref="Content">Content</see>. <br/>
+    /// If the file fails to load the original content will be returned.
+    /// </summary>
+    /// <returns>The loaded content.</returns>
+    public T? Load()
+    {
+        if (KeepOpen)
+        {
+            stream ??= File.Open(FilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+            using StreamReader reader = new(stream);
+            Content = JsonConvert.DeserializeObject<T>(reader.ReadToEnd()) ?? Content;
+        }
+        else
+        {
+            Content = JsonConvert.DeserializeObject<T>(File.ReadAllText(FilePath)) ?? Content;
+        }
+        return Content;
+    }
+
+    /// <summary>
+    /// Releases all resources used by the <see cref="ConfigurationFile{T}"/> object.
+    /// </summary>
+    public void Dispose()
+    {
+        Save();
+        stream?.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ Chase.CommonLib/FileSystem/Configuration/DatabaseFile.cs
@@ -0,0 +1,93 @@
+﻿/*
+    Chase CommonLib - LFInteractive LLC. 2021-2024
+    CommonLib is a library of common functions and classes for .NET 5.0+.
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
+using Newtonsoft.Json;
+using System.IO.Compression;
+
+namespace Chase.CommonLib.FileSystem.Configuration;
+
+/// <summary>
+/// A database file is a zip archive that contains a collection of entries.
+/// </summary>
+public class DatabaseFile : IDisposable
+{
+    private readonly ZipArchive baseStream;
+
+    /// <summary>
+    /// Creates a new database file.
+    /// </summary>
+    /// <param name="filePath"></param>
+    public DatabaseFile(string filePath)
+    {
+        baseStream = ZipFile.Open(filePath, ZipArchiveMode.Update);
+    }
+
+    /// <summary>
+    /// Creates or opens a database file.
+    /// </summary>
+    /// <param name="filePath"></param>
+    /// <returns></returns>
+    public static DatabaseFile Open(string filePath)
+    {
+        return new(filePath);
+    }
+
+    /// <summary>
+    /// Writes an entry to the database file.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    public void WriteEntry(Guid key, object value)
+    {
+        ZipArchiveEntry zipEntry = baseStream.GetEntry(ParseEntryPath(key)) ?? baseStream.CreateEntry(ParseEntryPath(key), CompressionLevel.SmallestSize);
+        using Stream stream = zipEntry.Open();
+        using StreamWriter writer = new(stream);
+        writer.Write(JsonConvert.SerializeObject(value));
+        writer.Flush();
+        stream.Flush();
+    }
+
+    /// <summary>
+    /// Reads an entry from the database file.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    public T? ReadEntry<T>(Guid key)
+    {
+        ZipArchiveEntry? zipEntry = baseStream.GetEntry(ParseEntryPath(key));
+        if (zipEntry != null)
+        {
+            using Stream stream = zipEntry.Open();
+            using StreamReader reader = new(stream);
+            return JsonConvert.DeserializeObject<T>(reader.ReadToEnd());
+        }
+        return default;
+    }
+
+    /// <summary>
+    /// Checks if an entry exists in the database file.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    public bool Exists(Guid key)
+    {
+        ZipArchiveEntry? zipEntry = baseStream.GetEntry(ParseEntryPath(key));
+        return zipEntry != null;
+    }
+
+    /// <summary>
+    /// Releases all resources used by the database file and writes them to file.
+    /// </summary>
+    public void Dispose()
+    {
+        baseStream.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    private static string ParseEntryPath(Guid key) => Path.Combine(key.ToString("N")[..2], key.ToString("N"));
+}
