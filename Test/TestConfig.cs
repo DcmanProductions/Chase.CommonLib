5062eb5606e7cdca6e439da0a42e9e456662f342
Drew Chase <dcmanproductions@gmail.com>
2023-09-16 23:40:50 -0400

    updated version

---
+++ Test/TestConfig.cs
@@ -0,0 +1,23 @@
+﻿/*
+    Chase CommonLib - LFInteractive LLC. 2021-2024
+    CommonLib is a library of common functions and classes for .NET 6.0+.
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
+using Chase.CommonLib.FileSystem.Configuration;
+
+namespace Test;
+
+public class TestConfig : AppConfigBase<TestConfig>
+{
+    public string TestString { get; set; } = "Hello World!";
+    public int TestInt { get; set; } = 123;
+    public bool TestBool { get; set; } = true;
+    public double TestDouble { get; set; } = 123.456;
+    public float TestFloat { get; set; } = 123.456f;
+    public decimal TestDecimal { get; set; } = 123.456m;
+    public Guid TestGuid { get; set; } = Guid.NewGuid();
+    public DateTime TestDateTime { get; set; } = DateTime.Now;
+    public TimeSpan TestTimeSpan { get; set; } = TimeSpan.FromMinutes(5);
+}
